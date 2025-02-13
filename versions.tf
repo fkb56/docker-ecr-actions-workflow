f43b4ad3a913364bf3c47f38ee60ff5881df064d
fkb56 <fkcb@live.fr>
2025-02-13 15:54:10 +0100

    init

--- versions.tf
+++ versions.tf
@@ -8,5 +8,12 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket = "myterraform56"
+    key    = "docker-ecr/terraform-fkb56_docker-ecr-actions-workflow.tfstate"
+    region = "eu-west-3"
+    assume_role = {
+      role_arn = "arn:aws:iam::794038241431:role/Terraform2"
+    }
+  }
 }
